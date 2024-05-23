0e06fe33f4c6328e7074654d0d1e44408660a73b
ptylczynski <ptylczynski@man.poznan.pl>
2024-05-23 19:12:34 +0200

    change: add pushing image to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 go build -o vikunja-emoji
-docker build -t vikunja-emoji .
+docker build -t ghcr.io/ptylczynski/vikunja-emoji:latest .
+docker push ghcr.io/ptylczynski/vikunja-emoji:latest
